d1d5fce88daa25907fc2f6bddaaba61b94a69a9d
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2025-06-29 14:50:45 +0000

    add jwt handler type

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,7 +1,131 @@
 use http::{HeaderMap, header};
-use std::pin::Pin;
-
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use p256::{
+    ecdsa::{SigningKey, VerifyingKey},
+    elliptic_curve::rand_core,
+    pkcs8::{EncodePrivateKey, EncodePublicKey},
+};
+use serde::{Serialize, de::DeserializeOwned};
+use std::{fs, path::PathBuf, pin::Pin};
 const JWT_IDENTIFIED: &str = "JWT";
+const ECDS_PRIV_FILE: &str = "ec_priv_key.pem";
+const ECDS_PUB_FILE: &str = "ec_pub_key.pem";
+const PKI: &str = "pki_auth";
+const ALGORITHM_JWT: Algorithm = Algorithm::ES256;
+
+pub struct JwtHandle;
+
+pub trait GenEcdsa {
+    fn gen_ecdsa() -> Result<(), JwtHandleError>;
+}
+
+pub trait VerifyTokenEcdsa {
+    fn verify_token<B>(token: &str) -> Result<B, JwtHandleError>
+    where
+        B: DeserializeOwned;
+}
+
+pub trait GenTokenFromEcds {
+    fn gen_token<T, B>(claim: T) -> Result<String, JwtHandleError>
+    where
+        T: Claims,
+        B: Serialize;
+}
+
+impl VerifyTokenEcdsa for JwtHandle {
+    fn verify_token<B>(token: &str) -> Result<B, JwtHandleError>
+    where
+        B: DeserializeOwned,
+    {
+        let mut path_pub_key = PathBuf::from(PKI);
+        path_pub_key.push(ECDS_PUB_FILE);
+        let pem_key = fs::read(path_pub_key).unwrap();
+        let dec = DecodingKey::from_ec_pem(&pem_key).unwrap();
+
+        Ok(decode::<B>(token, &dec, &Validation::new(ALGORITHM_JWT))
+            .unwrap()
+            .claims)
+    }
+}
+
+impl GenEcdsa for JwtHandle {
+    fn gen_ecdsa() -> Result<(), JwtHandleError> {
+        let key = SigningKey::random(&mut rand_core::OsRng);
+        let pub_key = VerifyingKey::from(&key);
+
+        let private_pem = key.to_pkcs8_pem(Default::default()).unwrap();
+        let public_pem = pub_key.to_public_key_pem(Default::default()).unwrap();
+        let path = PathBuf::from(PKI);
+
+        if !path.exists() {
+            fs::create_dir(&path).unwrap();
+        }
+
+        let mut path_privkey_pem = path.clone();
+        path_privkey_pem.push(ECDS_PRIV_FILE);
+
+        fs::write(path_privkey_pem, private_pem).unwrap();
+
+        let mut path_pubkey_pem = path;
+        path_pubkey_pem.push(ECDS_PUB_FILE);
+
+        fs::write(path_pubkey_pem, public_pem).unwrap();
+
+        Ok(())
+    }
+}
+
+impl GenTokenFromEcds for JwtHandleError {
+    fn gen_token<T, B>(claim: T) -> Result<String, JwtHandleError>
+    where
+        T: Claims,
+        B: Serialize,
+    {
+        let mut path_priv_key = PathBuf::from(PKI);
+        path_priv_key.push(ECDS_PRIV_FILE);
+
+        let priv_key = fs::read(path_priv_key).unwrap();
+        let priv_key = EncodingKey::from_ec_pem(&priv_key).unwrap();
+
+        let token = encode(
+            &Header::new(ALGORITHM_JWT),
+            &claim.get_claim::<B>(),
+            &priv_key,
+        )
+        .unwrap();
+        Ok(token)
+    }
+}
+
+impl JwtHandle {
+    pub fn verify_token<B>(token: String) -> Result<B, JwtHandleError>
+    where
+        B: DeserializeOwned,
+    {
+        let mut path = PathBuf::from(PKI);
+        path.push(ECDS_PUB_FILE);
+        let pub_key = fs::read(path).unwrap();
+
+        let dec = DecodingKey::from_ec_pem(&pub_key).unwrap();
+
+        let validate = Validation::new(ALGORITHM_JWT);
+
+        let decode = decode::<B>(&token, &dec, &validate).unwrap();
+        Ok(decode.claims)
+    }
+}
+
+pub trait Claims {
+    fn get_claim<T>(&self) -> T
+    where
+        T: Serialize;
+}
+
+#[derive(Debug)]
+pub enum JwtHandleError {
+    EnvNotFound,
+    GenEc,
+}
 
 pub trait GetToken {
     fn search(headers: &HeaderMap) -> Option<Self>
