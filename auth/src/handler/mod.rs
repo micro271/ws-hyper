e839cc002e175505b0ec45f94fc08c04b99151ad
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2025-10-02 06:57:30 -0300

    Add endpoints for the methods PATH, POST, and DELETE to users

--- auth/src/handler/mod.rs
+++ auth/src/handler/mod.rs
@@ -15,8 +15,8 @@ use uuid::Uuid;
 
 use crate::{
     handler::error::ResponseErr,
-    models::user::Claim,
-    repository::PgRepository,
+    models::user::{Claim, User},
+    repository::{PgRepository, QueryOwn},
 };
 
 type ResponseHandlers = Result<Response<Full<Bytes>>, ResponseErr>;
@@ -56,6 +56,20 @@ pub async fn api(req: Request<Incoming>) -> ResponseHandlers {
         ("user", &Method::GET) => {
             let id = req.extensions().get::<Claim>().unwrap().sub;
             user::get(req, id).await
+        }
+        ("user", method @ ( &Method::POST | &Method::PATCH | &Method::DELETE) ) => {
+            let id = req.extensions().get::<Claim>().unwrap().sub;
+            let repo = req.extensions().get::<Repo>().unwrap();
+            let user = repo.get::<User>(QueryOwn::builder().wh("id", id.into())).await?;
+            if !user.is_admin() {
+                return Err(ResponseErr::status(StatusCode::UNAUTHORIZED));
+            }
+            match method {
+                &Method::PATCH => user::update(req, id).await,
+                &Method::POST => user::new(req).await,
+                &Method::DELETE => user::delete(req, id).await,
+                _ => Err(ResponseErr::status(StatusCode::INTERNAL_SERVER_ERROR))
+            }
         }
         ("users", &Method::GET) => user::get_all(req).await,
         ("users/info", &Method::GET) => user::get_user_info(req, None).await,
