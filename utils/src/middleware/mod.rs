9e71a93faabec1a5f90e4d8ee0a7adc430f8e3a6
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2025-12-07 13:33:19 -0300

    creade Middleware trait

--- utils/src/middleware/mod.rs
+++ utils/src/middleware/mod.rs
@@ -1,1 +1,10 @@
 pub mod cors;
+use http::{Request, Response};
+use hyper::body::Body;
+
+pub trait Middleware: Sync {
+    type ReqBody: Body + Send;
+    type ResBody: Body + Default + Send;
+
+    fn middleware(&self, req: Request<Self::ReqBody>) -> impl Future<Output = Response<Self::ResBody>> + Send;
+}
